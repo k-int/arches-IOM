59b2d407778c5e58421338901fb0c66ec710fa16
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2024-07-10 14:44:08 +0100

    Correct migration flow

--- arches/app/models/migrations/7787_relational_data_model.py
+++ arches/app/models/migrations/7787_relational_data_model.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "7128_resource_instance_filter"),
+        ("models", "7129_latestresourceedit"),
     ]
 
     operations = [
